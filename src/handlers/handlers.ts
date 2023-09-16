9caae4c7be81b2a921c7db2da7e85d5b6c9ad26c
shuji okamura <42924899+beex-okamura@users.noreply.github.com>
2023-09-16 11:56:02 +0900

    fix

--- src/handlers/handlers.ts
+++ src/handlers/handlers.ts
@@ -2,17 +2,34 @@ import {type SQSEvent} from 'aws-lambda';
 import logger from '../lib/logger.ts';
 import {getBrowser} from '../lib/browser.ts';
 import {JobCanClient} from '../playwright/jobcan.ts';
-import {type Page} from 'playwright';
+import {type Browser, type Page} from 'playwright';
+import {type UserInfo} from '../entities/user.ts';
 
 export const handler = async (event: SQSEvent) => {
-	const messages = event.Records.map(e => JSON.parse(e.body));
+	const messages = event.Records.map(e => JSON.parse(e.body) as UserInfo);
 
 	logger.info('start jobcan scraping');
 	const browser = await getBrowser();
-	const page = await browser.newPage();
+
+	try {
+		for (const message of messages) {
+			const {userId, password} = message;
+
+			// eslint-disable-next-line no-await-in-loop
+			await workPunch(browser, userId, password);
+		}
+	} finally {
+		await browser.close();
+	}
+
+	logger.info('end jobcan scraping');
 };
 
-export const workPunch = async (page: Page, userId: string, password: string) => {
+export const workPunch = async (browser: Browser, userId: string, password: string, dryRun = false) => {
+	const page = await browser.newPage();
+
 	const jobcan = new JobCanClient(page);
 	await jobcan.login(userId, password);
+
+	await jobcan.workPunch();
 };
--- src/playwright/jobcan.ts
+++ src/playwright/jobcan.ts
@@ -1,11 +1,48 @@
 import {type Page} from 'playwright';
+import {format} from 'date-fns';
+import logger from '../lib/logger.ts';
+import path from 'path';
+import {getEnvironments} from '../lib/environments.ts';
+import { addPrefixToS3Path, uploadToS3 } from '../lib/s3.ts';
 
-const BASE_URL = 'https://id.jobcan.jp/users/sign_in';
+const nowDate = new Date();
+const {uploadBucket} = getEnvironments();
 
 export class JobCanClient {
-	constructor(private readonly page: Page) { }
+	constructor(
+		private readonly page: Page,
+		private readonly downloadDir?: string,
+	) { }
 
 	async login(userId: string, password: string) {
-		await this.page.goto(BASE_URL);
+		await this.page.goto('https://id.jobcan.jp/users/sign_in');
+		await this.page.fill('#user_email', userId);
+		await this.page.fill('#user_password', password);
+		await this.page.click('input[type="submit"]');
+
+		await this.page.waitForResponse(
+			resp => resp.url()
+				.includes('/account/profile/atd_admin_link_disp') && resp.status() === 200,
+		);
+	}
+
+	async workPunch() {
+		await this.page.goto('https://ssl.jobcan.jp/jbcoauth/login');
+
+		return this.page.innerText('#working_status');
+	}
+
+	async saveSnapshotAndThrowError(err: Error) {
+		logger.error(err.stack);
+		const snapshotName = `${format(nowDate, 'yyyyMMdd_hhmmss')}.png`;
+		const body = await this.page.screenshot({
+			path: path.join(this.downloadDir ?? '', snapshotName),
+			type: 'png',
+		});
+		if (uploadBucket !== undefined) {
+			await uploadToS3(uploadBucket, addPrefixToS3Path('Snapshot', snapshotName), body);
+		}
+
+		throw err;
 	}
 }
