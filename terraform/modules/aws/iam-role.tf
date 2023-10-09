d2a8f3633a6b594a4162b792fe511b0d18490067
shuji okamura <42924899+beex-okamura@users.noreply.github.com>
2023-10-09 10:27:31 +0000

    fix

--- terraform/modules/aws/iam-role.tf
+++ terraform/modules/aws/iam-role.tf
@@ -24,3 +24,8 @@ resource "aws_iam_role_policy_attachment" "jobcan-scraping-sqs-lambda" {
   role       = aws_iam_role.jobcan-basic-lambda.name
   policy_arn = aws_iam_policy.jobcan-scraping-sqs-lambda-queue-policy.arn
 }
+
+resource "aws_iam_role_policy_attachment" "jobcan-scraping-s3-scraping-capture" {
+  role       = aws_iam_role.jobcan-basic-lambda.name
+  policy_arn = aws_iam_policy.jobcan-scraping-s3-scraping-capture-policy.arn  
+}
